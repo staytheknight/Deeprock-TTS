0b8303b4eab48e7e492c47b387b5af4121b987cd
Elise Chevalier <staytheknight@gmail.com>
2025-08-01 11:53:26 -0600

    Space Rig Locs

---
+++ Space Rig Expansion.248e97.lua
@@ -0,0 +1,88 @@
+local locations = 
+{
+    rulebookLoc = {-54.06, 0.97, -50.58},
+    rulebookRot = {0.00, 180.0, 0.00},
+
+    rulebookAid1Loc = {-60.28, 0.97, 23.94},
+    rulebookAid2Loc = {-54.59, 0.97, 23.97},
+    rulebookAid3Loc = {-48.94, 0.97, 24.06},
+    rulebookAid4Loc = {-43.15, 0.97, 24.02},
+    rulebookAid5Loc = {-37.46, 0.97, 24.02},
+    rulebookAid6Loc = {-60.26, 0.97, 15.76},
+    rulebookAid7Loc = {-54.62, 0.97, 15.84},
+    rulebookAid8Loc = {-48.98, 0.97, 15.84},
+    rulebookAid9Loc = {-43.27, 0.97, 15.85},
+    rulebookAid10Loc = {-37.56, 0.97, 15.82},
+    rulebookAidRot = {0.00, 180.03, 0.00},
+
+    oxygenTokenLoc = {{-22.25, 1.04, -21.33},{-6.02, 1.04, -21.42},{10.40, 1.04, -21.34},{24.88, 1.04, -21.61}},
+    oxygenTokenRot = {0.00, 180.0, 0.0},
+
+    upgradeBoardGunnerLoc = {-24.20, 1.12, -17.71},
+    upgradeBoardScoutLoc = {-8.76, 1.07, -17.69},
+    upgradeBoardDrillerLoc = {6.60, 1.07, -17.69},
+    upgradeBoardEngineerLoc = {21.98, 1.11, -17.70},
+    upgradeBoardRot = {0.0, 90.0, 0.0},
+
+    spaceRigBoardLoc = {-43.44, 1.07, 1.15},
+    spaceRigBoardRot = {0.00, 180.0, 0.00},
+
+    challengeDeckLoc = {-46.00, 2.0, 3.84},
+    challengeDeckRot = {0.00, 180.00, 180.00},
+
+    beerDeckLoc = {-38.03, 2.0, 3.50},
+    beerDeckRot = {0.00, 180.0, 180.00}, 
+
+    stickerExterminators = {-54.99, 1.04, 4.01},
+    stickerSuperProtector = {-53.89, 1.05, 4.01},
+    stickerUpClose = {-52.79, 1.04, 4.01},
+    stickerBreathtaking = {-51.69, 1.04, 4.01},
+    stickerHardboiled = {-55.54, 1.05, 3.06},
+    stickerHoarders = {-54.44, 1.05, 3.06},
+    stickerRoughnecks = {-53.34, 1.05, 3.06},
+    stickerVeteranDwarves = {-52.24, 1.04, 3.06},
+    stickerRot = {0.0, 180.0, 0.0}, 
+
+    missionModRedLoc = {-34.65, 0.97, 1.15},
+    missionModRedRot = {0.0,0.0,0.0},
+    missionModYellowLoc = {-34.65, 0.97, -0.75},
+    missionModYellowRot = {0.00, 180.00, 0.00},
+
+    upgradesHUDBagLoc = {-54.44, 0.97, 1.15},
+    upgradesArmourBagLoc = {-53.34, 0.97, 1.15},
+    upgradesPickaxeBagLoc = {0.00, 0.87, 0.00},
+    upgradesRot = {0.00, 180.00, 0.00},
+
+    pickupShuffleBagLoc = {12.10, 0.97, -7.42},
+    pickupAbandonedGearBagLoc = {13.20, 0.97, -7.42},
+    pickupGoldBugBagLoc = {14.30, 0.97, -7.42},
+    pickupMaltSaltBagLoc = {15.40, 0.97, -7.42},
+    pickupYeastBagLoc = {16.50, 0.97, -7.42},
+    pickupRot = {0.0,0.0,0.0},
+
+    pumpjackBagLoc = {-14.85, 0.96, 3.06},
+    pumpjackBagRot = {0.00, 180.00, 0.00},
+    pipeStraightBagLoc = {-14.85, 0.97, 1.15},
+    pipeStraightBagRot = {0.0,0.0,0.0},
+    pipeCurvedBagLoc = {-14.85, 0.97, -0.75},
+    pipeCurvedBagRot = {0.00, 240.00, 0.00},
+
+    drilldozerLoc = {-24.75, 0.97, 1.15},
+    drilldozerRot = {0.00, 270.0, 0.00},
+    drilldozerTokenLoc = {-22.00, 1.05, -1.70},
+    drilldozerTokenRot = {0.00, 180.0, 0.00},
+
+    refineryLoc = {-15.95, 1.11, -2.66},
+    refineryRot = {0.00, 300.00, 180.00},
+
+    beamerBagLoc = {-19.25, 0.96, 1.15},
+    beamerBagRot = {0.00, 90.00, 0.00},
+
+    heartstoneLoc = {-21.45, 0.97, 1.15},
+    heartstoneRot = {0.00, 239.96, 0.00},
+    heartstoneGemLoc = {-21.41, 1.37, 1.28},
+    hearstoneGemRot = {50.92, 83.33, 192.57},
+
+    miniHiddenCavesBagLoc = {16.50, 0.95, 0.20},
+    miniHiddenCavesBagRot = {0.00, 60.00, 0.00},
+}
